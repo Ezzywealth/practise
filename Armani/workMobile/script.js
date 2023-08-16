02ff344e9a08ee2e0c1e475c7567b739e3de930b
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-08-17 00:49:03 +0100

    Add some styles

--- Armani/workMobile/script.js
+++ Armani/workMobile/script.js
@@ -1,16 +1,31 @@
 // const loginStart = document.getElementById('loginStart');
 const loginStart = document.querySelector('.login_normal');
-const loginBtn = document.getElementById('loginBtn');
-const container1 = document.querySelector('.page1');
-const container2 = document.querySelector('.page2');
+const loginBtn = document.getElementById('loginBtn2');
+const container1 = document.getElementById('section1');
+const container2 = document.getElementById('section2');
+const emailInput = document.getElementById('login_param');
+const passwordInput = document.getElementById('password');
+const inputId = document.getElementById('email_label');
 
-console.log(container1);
+let email = '';
+
 loginStart.addEventListener('click', () => {
 	container1.style.display = 'none';
 	container2.style.display = 'flex';
+	email = emailInput.value;
+	inputId.value = email;
 });
 
 loginBtn?.addEventListener('click', () => {
-	container2.style.display = 'none';
-	container1.style.display = 'flex';
+	console.log(email);
+	console.log(passwordInput.value);
+});
+
+emailInput.addEventListener('input', () => {
+	loginStart.style.backgroundColor = 'green';
+	loginStart.removeAttribute('disabled');
+});
+
+passwordInput.addEventListener('input', () => {
+	loginBtn.removeAttribute('disabled');
 });
