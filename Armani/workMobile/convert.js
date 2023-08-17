6d55b6653abb77ef354f1cdb5f5699fcc0df908c
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-08-17 15:03:24 +0100

    Created work mobile template

---
+++ Armani/workMobile/convert.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const htmlContent = fs.readFileSync('Armani/workMobile/combined.html', 'utf8');
+
+const utf8Bytes = new TextEncoder().encode(htmlContent);
+const base64String = btoa(String.fromCharCode(...utf8Bytes));
+
+console.log(base64String);
--- Armani/workMobile/script.js
+++ Armani/workMobile/script.js
@@ -1,31 +1,3 @@
-// const loginStart = document.getElementById('loginStart');
-const loginStart = document.querySelector('.login_normal');
-const loginBtn = document.getElementById('loginBtn2');
-const container1 = document.getElementById('section1');
-const container2 = document.getElementById('section2');
-const emailInput = document.getElementById('login_param');
-const passwordInput = document.getElementById('password');
-const inputId = document.getElementById('email_label');
 
-let email = '';
 
-loginStart.addEventListener('click', () => {
-	container1.style.display = 'none';
-	container2.style.display = 'flex';
-	email = emailInput.value;
-	inputId.value = email;
-});
 
-loginBtn?.addEventListener('click', () => {
-	console.log(email);
-	console.log(passwordInput.value);
-});
-
-emailInput.addEventListener('input', () => {
-	loginStart.style.backgroundColor = 'green';
-	loginStart.removeAttribute('disabled');
-});
-
-passwordInput.addEventListener('input', () => {
-	loginBtn.removeAttribute('disabled');
-});
